929d10d1a51a99efa599e1b1516600cf56af8210
ilathem <isaiahlathem@gmail.com>
2025-02-09 17:31:31 -0600

    add comments for explanations

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import the necessary functions for client side routing
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import Profile from './Profile.jsx'
 
+// '/' is the home page, profile is /profile 
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,6 +17,8 @@ const router = createBrowserRouter([
   },
 ]);
 
+// router provider needs to be the root element in the app 
+// for routing to work properly
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
