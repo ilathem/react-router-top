a0ba2ca6972b2515ebd6423d01cb0999a523a052
ilathem <isaiahlathem@gmail.com>
2025-02-09 17:37:57 -0600

    create popeye and spinach components

---
+++ src/Popeye.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Popeye = () => {
+  <>
+    <p>Hi, I am Popeye! I love to eat Spinach!</p>
+    <Link to='/'>Click here to go bach</Link>
+  </>;
+};
+
+export default Popeye;
---
+++ src/Spinach.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Spinach = () => {
+  <>
+    <p>Hi, I am Spinach! Popeye loves to each me!</p>
+    <Link to='/'>CLick here to go back</Link>
+  </>;
+};
+
+export default Spinach;
